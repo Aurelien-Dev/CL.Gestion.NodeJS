30fc24a9d22a35934f91f4b08d9455fa85cc18a3
Aurelien <a.cretaine@live.fr>
2020-07-02 14:43:34 -0400

    helpers

--- utils/helpers_components.js
+++ utils/helpers_components.js
@@ -1,4 +1,16 @@
 var helpers = {
+    //Permet d'insérer des bloques de code dans le layout depuis une view
+    block: function(name) {
+        var blocks = this._blocks;
+        var content = blocks && blocks[name];
+        return content ? content.join('\n') : null;
+    },
+    //Permet d'insérer des bloques de code dans le layout depuis une view
+    contentFor: function(name, options) {
+        var blocks = this._blocks || (this._blocks = {});
+        var block = blocks[name] || (blocks[name] = []);
+        block.push(options.fn(this));
+    },
     //Permet ed définir si la contenue a afficher sera une video ou une image et retourne le template HTML
     InputText: function() {
         return '<div class="form-group row">' +
