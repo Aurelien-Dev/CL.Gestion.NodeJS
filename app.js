42deda19cb94878b50fbbe6ad5ff1d1d0cffee1e
Aurelien <a.cretaine@live.fr>
2020-07-25 00:26:59 -0400

    test

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.set('views', path.join(__dirname, "/views"));
 
 //Ouverture d'un dossier public
 app.use(express.static(path.join(__dirname + '/public')));
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
--- controller/homeController.js
+++ controller/homeController.js
@@ -7,8 +7,10 @@ const router = express.Router();
  ** Affichage d'accueil
  */
 router.get('/', function(request, response) {
+    response.render('home/tableau');
+
     formulaireDB.getFormulaires((formulaires) => {
-        response.render('home/tableauBord', { frm: formulaires });
+        response.render('home/tableau', { frm: formulaires });
     });
 });
 
