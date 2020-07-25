77864623bd32c633225936780c3e71e30043fe03
Aurelien <a.cretaine@live.fr>
2020-07-25 00:04:09 -0400

    essai

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('view engine', 'handlebars');
 //app.set('view', path.join(__dirname, "views"));
 
 //Ouverture d'un dossier public
-app.use(express.static('public/'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
