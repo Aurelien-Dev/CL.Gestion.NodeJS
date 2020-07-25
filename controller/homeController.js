680ac53b2cf9de04b27069a1754ed8e20de626bb
Aurelien <a.cretaine@live.fr>
2020-07-24 23:56:08 -0400

    logs

--- controller/homeController.js
+++ controller/homeController.js
@@ -8,6 +8,7 @@ const router = express.Router();
  */
 router.get('/', function(request, response) {
     formulaireDB.getFormulaires((formulaires) => {
+        console.log(formulaires);
         response.render('home/tableauBord', { formulaires: formulaires });
     });
 });
