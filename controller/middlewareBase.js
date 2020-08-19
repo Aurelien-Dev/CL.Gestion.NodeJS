19ec8766e9843b97f4960b5f1e7f1516896c2180
Aurelien <a.cretaine@live.fr>
2020-08-19 12:43:29 -0400

    complément information

--- controller/middlewareBase.js
+++ controller/middlewareBase.js
@@ -4,7 +4,15 @@ const express = require('express');
 const router = express.Router();
 const _ = require('underscore');
 
-
+/**
+ * Middleware permettant d'ajouter les informations de compteur automatiquement au changement de la page
+ * 
+ * Aide utilisé :
+ * https://stackoverflow.com/questions/9285880/node-js-express-js-how-to-override-intercept-res-render-function
+ * @param {object} req Request
+ * @param {Object} res Response
+ * @param {callback} next Passage au middleware suivant
+ */
 function middlewareObtentionCompteur(req, res, next) {
     utilitaireDB.getCompteurs((compteur) => {
         // grab reference of render
