fa106007de926d478efd23c8abd2831c2d995332
Aurelien <a.cretaine@live.fr>
2020-08-24 21:40:42 -0400

    Retirer les membres supprimé du compteur

--- db/utilitaireDB.js
+++ db/utilitaireDB.js
@@ -15,7 +15,7 @@ const getMembresAutoComplete = (callback) => {
 };
 
 const getCompteurs = (callback) => {
-    db.query(`select (select count(*) from membre m2 ) as count_membres, 
+    db.query(`select (select count(*) from membre where est_supprime = false) as count_membres, 
                      (select count(*) from formulaire_risque fr where fr.numero_sequence_membre  is null) as count_formulaire`, (error, results) => {
         if (error) {
             return next(error);
