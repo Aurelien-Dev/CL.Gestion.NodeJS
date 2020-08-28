44d5094b274c954c106d3e57bcc4d2770b2b89c5
Aurelien <a.cretaine@live.fr>
2020-08-28 09:52:04 -0400

    centrage besoin, pas activité mais fich d'inscr

--- db/activiteDB.js
+++ db/activiteDB.js
@@ -3,11 +3,9 @@ const db = require('../db/baseDB');
 const getActivites = (callback) => {
     db.query(`select numero_sequence,
                      nom,
-                     description,
-                     difficulte,
-                     volet,
                      to_char(date_heure_debut, 'YYYY-MM-DD HH24hMI') as date_heure_debut,
-                     to_char(date_heure_fin, 'YYYY-MM-DD HH24hMI') as date_heure_fin
+                     to_char(date_heure_fin, 'YYYY-MM-DD HH24hMI') as date_heure_fin,
+                     url
                 from public.activite`, (error, results) => {
         if (error) {
             return next(error);
@@ -19,11 +17,9 @@ const getActivites = (callback) => {
 const getActiviteByNumeroSequence = (id, callback) => {
     db.query(`select numero_sequence,
                      nom,
-                     description,
-                     difficulte,
-                     volet,
                      to_char(date_heure_debut, 'YYYY-MM-DD HH24hMI') as date_heure_debut,
-                     to_char(date_heure_fin, 'YYYY-MM-DD HH24hMI') as date_heure_fin
+                     to_char(date_heure_fin, 'YYYY-MM-DD HH24hMI') as date_heure_fin,
+                     url
                 from public.activite
                where numero_sequence = $1`, [id], (error, results) => {
         if (error) {
