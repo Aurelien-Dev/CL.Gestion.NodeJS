e8b6cbaa180b0d54c9bb18611d0b173d659d328b
Nicolas Beaumont-Frenette <daboss@ballotsrus.com>
2021-01-04 11:59:12 -0500

    Progrès... je pense

--- controllerAPI/calculetteController.js
+++ controllerAPI/calculetteController.js
@@ -6,9 +6,28 @@ const router = express.Router();
 const async = require('async');
 const { getMaxListeners } = require('../app');
 
-router.post('/api/calculette/membre', function(request, response) {
+router.post('/api/calculette/participant', function(request, response) {
     console.log(request.body.nom);
     request.session.maliste.push({nom: request.body.nom});
+    request.session.listeDepenses.forEach(function (item, index) {
+        item.participants.push(true);
+    });   
+    response.status(200).json({ success: true });
+});
+
+router.post('/api/calculette/depense', function(request, response) {
+    console.log(request.body);
+    request.session.listeDepenses.push(request.body);
+    response.status(200).json({ success: true });
+});
+
+router.delete('/api/calculette/participant/:id', function(request, response) {
+
+    request.session.listeDepenses.forEach(function (item, index) {
+        item.participants.splice(request.params.id,1);
+    });
+    request.session.maliste.splice(request.params.id,1);
+
     response.status(200).json({ success: true });
 });
 
--- public/js/home/calculette.js
+++ public/js/home/calculette.js
@@ -1,24 +1,27 @@
 $(function(window, $) {
     if ('undefined' == typeof(window.CL.View.tableauBord)) { window.CL.View.tableauBord = {}; }
 
-    const SELECTOR_SUPP_MBR = '.supprimer-membre';
+    const SELECTOR_SUPP_PARTICIPANT = '.supprimer-participant';
     const SELECTOR_TBL_MEMBRES = '#tbl-membres';
-    const SELECTOR_FRM_ENR_ASSO = '#frmAjouterParticipant';
-    const SELECTOR_MDL_CREER_ADH = '#modalAjouterParticipant';
-    const SELECTOR_LIER_FRM = '#ajouterParticipant';
+    const SELECTOR_FRM_AJT_PARTICIPANT = '#frmAjouterParticipant';
+    const SELECTOR_MDL_AJT_PARTICIPANT = '#modalAjouterParticipant';
+    const SELECTOR_AJT_PARTICIPANT = '#ajouterParticipant';
+    const SELECTOR_FRM_AJT_DEPENSE = '#frmAjouterDepense';
+    const SELECTOR_AJT_DEPENSE = '#ajouterDepense';
+    const SELECTOR_MDL_AJT_DEPENSE = '#modalAjouterDepense';
 
-    var modalSupMembre = new window.CL.Utilitaires.Modal({
+    var modalSupParticipant = new window.CL.Utilitaires.Modal({
         titre: 'Suppression',
-        body: 'Veux-tu supprimer le membre ?',
+        body: 'Veux-tu supprimer le participant ?',
         boutons: [{
             texte: 'Oui',
             callback: function() {
-                desactiverMembre(modalSupMembre.ligne);
+                desactiverMembre(modalSupParticipant.ligne);
             }
         }, {
             texte: 'Non',
             callback: function() {
-                modalSupMembre.CacherModal();
+                modalSupParticipant.CacherModal();
             }
         }]
     });
@@ -29,9 +32,11 @@ $(function(window, $) {
      * Initialisation de la page
      */
     function initialiserPage() {
-        $(SELECTOR_SUPP_MBR).click(eventClickSupprimerMembre);
-        $(SELECTOR_FRM_ENR_ASSO).submit(eventEnregistrerParticipant);
-        $(SELECTOR_LIER_FRM).click(eventClickModalAjouterParticipant);
+        $(SELECTOR_SUPP_PARTICIPANT).click(eventClickSupprimerParticipant);
+        $(SELECTOR_FRM_AJT_PARTICIPANT).submit(eventEnregistrerParticipant);
+        $(SELECTOR_AJT_PARTICIPANT).click(eventClickModalAjouterParticipant);
+        $(SELECTOR_FRM_AJT_DEPENSE).submit(eventEnregistrerDepense);
+        $(SELECTOR_AJT_DEPENSE).click(eventClickModalAjouterDepense);
 
         initialiserDatatale();
     }
@@ -52,41 +57,39 @@ $(function(window, $) {
      * Evenement permettant de faire la suppression d'une adhésion
      * @param {jQuery} e event object
      */
-    function eventClickSupprimerMembre(e) {
+    function eventClickSupprimerParticipant(e) {
         e.preventDefault();
 
         $that = $(this);
-        modalSupMembre.ligne = $that.parents('tr');
-        modalSupMembre.AfficherModal();
+        modalSupParticipant.ligne = $that.parents('tr');
+        modalSupParticipant.AfficherModal();
     }
 
     function eventClickModalAjouterParticipant(e) {
         $that = $(this);
 
-        modalInstance = $(SELECTOR_MDL_CREER_ADH).modal();
+        modalInstance = $(SELECTOR_MDL_AJT_PARTICIPANT).modal();
 
-        //var ligne = $(e.currentTarget).parents('tr');
-        //var info_fiche = ligne.data('info-fiche');
-        //var seq_fiche = ligne.data('seq-fiche');
+    }
 
-        //$('#infoFiche').html(info_fiche);
-        //modalInstance.ligne = ligne;
+    function eventClickModalAjouterDepense(e) {
+        $that = $(this);
+        console.log('modal!');
+        modalInstance = $(SELECTOR_MDL_AJT_DEPENSE).modal();
+
     }
 
     function eventEnregistrerParticipant(e) {
         e.preventDefault();
-        modalAjouterParticipant = $(SELECTOR_MDL_CREER_ADH).modal();
+        modalAjouterParticipant = $(SELECTOR_MDL_AJT_PARTICIPANT).modal();
 
         var ligne = $('#nom').val()
-        var href = '/api/calculette/membre';
-        console.log(href);
-        console.log(ligne);
+        var href = '/api/calculette/participant';
         $.ajax({
             url: href,
             method: 'post',
             data: {nom: ligne},
             success: function(result, statut) {
-                console.log('un succès');
                 location.reload();
             }
         });
@@ -94,18 +97,41 @@ $(function(window, $) {
         //alert('stop');
     }
 
+    function eventEnregistrerDepense(e) {
+        e.preventDefault();
+        modalAjouterParticipant = $(SELECTOR_MDL_AJT_DEPENSE).modal();
+
+        var participants = [];
+        $("input:checkbox[name=participants]").each(function() {participants.push($(this).prop("checked"));
+        });
+        var ligne = {description: $('#description').val(),
+                    montant: $('#montant').val(),
+                    participants: participants}
+        var href = '/api/calculette/depense';
+        console.log(ligne);
+        $.ajax({
+            url: href,
+            method: 'post',
+            data: ligne,
+            success: function(result, statut) {
+                location.reload();
+            }
+        });
+        
+    }
+
     /**
      * Permet d'effectuer la modification du rôle du membre
      * @param {$ligne} ligne Élément jQuery qui correspond à la ligne du membre
      */
     function desactiverMembre(ligne) {
-        var href = $(ligne).find(SELECTOR_SUPP_MBR).attr('href');
-
+        var href = $(ligne).find(SELECTOR_SUPP_PARTICIPANT).attr('href');
+        console.log(href);
         $.ajax({
             url: href,
-            method: 'PUT',
+            method: 'DELETE',
             success: function(result, statut) {
-                modalSupMembre.CacherModal();
+                modalSupParticipant.CacherModal();
                 $(ligne).hide('slow');
             }
         });
